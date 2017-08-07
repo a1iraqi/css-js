6b6801b215a313e43465a4140b9812a1c433c796
a1iraqi <abood.fi123@gmail.com>
2017-08-07 11:25:07 +0300

    Add files via upload

---
+++ muen.js
@@ -0,0 +1,1 @@
+var myApp=new Framework7();var $$=Dom7;var view1=myApp.addView('#view-1',{dynamicNavbar:true});var view2=myApp.addView('#view-2',{dynamicNavbar:true});var view3=myApp.addView('#view-3',{dynamicNavbar:true});
